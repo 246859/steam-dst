53640dac45c02acf95e841a1c87187be244e79f5
yihhao wang <2633565580@qq.com>
2024-06-10 17:13:56 +0800

    chore(setup): update build info

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read() \
-        .replace('.io/en/latest/', '.io/en/stable/') \
-        .replace('?badge=latest', '?badge=stable') \
-        .replace('projects/steam/badge/?version=latest', 'projects/steam/badge/?version=stable')
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = f.read() \
+#         .replace('.io/en/latest/', '.io/en/stable/') \
+#         .replace('?badge=latest', '?badge=stable') \
+#         .replace('projects/steam/badge/?version=latest', 'projects/steam/badge/?version=stable')
 with open(path.join(here, 'steam/__init__.py'), encoding='utf-8') as f:
     __version__ = f.readline().split('"')[1]
 
@@ -37,10 +37,10 @@ setup(
     name='steam-dst',
     version=__version__,
     description='Module for interacting with various Steam features',
-    long_description=long_description,
-    url='https://github.com/ValvePython/steam',
-    author="Rossen Georgiev",
-    author_email='rossen@rgp.io',
+    long_description='hot fix for package steam bug of manifest_gid',
+    url='https://github.com/246859/steam-dst',
+    author="246859",
+    author_email='2633565580@qq.com',
     license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
